1fad18f3d84fd9041662739e93369044b0dcfe11
Adama Asenath <169082427+HopeAda@users.noreply.github.com>
2026-05-13 18:16:13 +0100

    Designed the Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
 	return (
-		<div className="flex-center h-screen">
-			<h1 className="text-3xl text-amber-300">Hello</h1>
-		</div>
+		<main>
+			<Navbar />
+		</main>
 	);
 };
 
